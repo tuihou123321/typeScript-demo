91e65aafd5c050a205bbb2b053aa54ef91cd5ed3
zhuqiao <chrome0423@gmail.com>
2021-05-26 20:02:19 +0800

    update

--- demo/类型断言/断言在联合类型中的使用.js
+++ demo/类型断言/断言在联合类型中的使用.js
@@ -1,4 +1,15 @@
+// 断言在函数中的运用
 // 使用 (<string>.something).length 来断言
+function getLength(something) {
+    // 使用<>来断言
+    if (something.length) {
+        return something.length;
+    }
+    else {
+        return something.toString().length;
+    }
+}
+// 使用as
 function getLength3(something) {
     // 使用as语法来断言
     if (something.length) {
--- demo/类型断言/断言的两种方式.ts
+++ demo/类型断言/断言的两种方式.ts
@@ -4,6 +4,7 @@ window.a=10;   // 报错 window 对象定义a 这个属性
 (window as any).a=10;   // 使用any断言，正常显示
 
 
+// 局部变量
 const IMSDK={
     // 获取通讯录列表
     getConcact(){
