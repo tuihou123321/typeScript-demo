ba156f5d31bc2db8b34f7fb527e86e300680fc88
zhuqiao <chrome0423@gmail.com>
2021-11-24 14:46:36 +0800

    update:typeof

--- demo/命令空间/命令空间-嵌套命令空间-取值1.ts
+++ demo/命令空间/命令空间-嵌套命令空间-取值1.ts
@@ -1,7 +1,7 @@
 // 跨文件获取全局这是需要引入对应路径
 /// <reference path = "命令空间-嵌套命令空间.d.ts" />
 
-// 虽然Colums10没有导出，但是IDE能提示，但是不推荐不写export
+// 自动导入
 import ColumsChild1 = TableNested.Colums.ColumsChild1;
 
 let obj : TableNested.Colums.ColumsChild1 = {
--- demo/操作符/typeof-获取变量类型-引用类型.ts
+++ demo/操作符/typeof-获取变量类型-引用类型.ts
@@ -1,20 +1,28 @@
-// 定义一个类型
-type OptionItem = {
-    value:string;
-    label:string;
-}[]
-// 定义一个变量，并规定类型
-let optionItem : OptionItem;
-// 创建一个新的类型，通过typeOf 变量来获得它的类型
-// 能过typeOf 获取到变量或对象的类型（注意这是不能是type ,interface等定义的接口，也不能是泛型中的变量）
-type ObjType = typeof  optionItem;
-
-
-// 定义一个新的变量，这个变量只能接收指定的类型参数
-let obj:ObjType;
-obj = 1; // 报错类型不符
-
-obj = [{
+const defaultArr = [{
     value:'a',
     label:'a',
 }]
+
+// 通过typeof 获取到对象的类型
+type ObjType = typeof defaultArr;
+
+
+const obj1: ObjType = [{
+    value:'a',
+    label:'a',
+}];
+
+
+const obj2: ObjType = [{
+    value:1,    // 报错类型不符
+    label:'a',
+}];
+
+
+const obj3: ObjType = [{
+    value:'a',
+    label:'a',
+    name:'a'    // 未定义的参数报错
+}];
+
+
